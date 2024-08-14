5ca82ca950080e194c42479bb0f1f6d64b71b28e
atusveld <alextusv@gmail.com>
2024-08-14 14:00:56 +0200

    Added protection to redirections.

--- testshell/Src/Execution/redirect.c
+++ testshell/Src/Execution/redirect.c
@@ -17,32 +17,21 @@ void	ft_red_out(t_parse *parsed)
 	int	fd;
 	
 	if (!parsed || !parsed->redir_out)
-        return;
-	if (parsed->redir_out->type == APPEND)
-		ft_append(parsed);
-	if (parsed->redir_out->next)
+        ft_error("red_out, parsed", 1);
+	while (parsed->redir_out)
 	{
-		while (parsed->redir_out)
+		if (parsed->redir_out->type == APPEND)
+			ft_append(parsed);
+		if (parsed->redir_out->next)
 		{
 			fd = ft_create_file(parsed);
 			if (fd == -1)
-			{
 				ft_error("fd, create file", 1);
-				return ;
-			}
-			parsed->redir_out = parsed->redir_out->next;
+			ft_write_to_file(fd, parsed);
+			if (close(fd) == -1)
+				ft_error("close, create file", 1);
 		}
-		ft_write_to_file(fd, parsed);
-	}
-	else
-	{
-		fd = ft_create_file(parsed);
-		if (fd == -1)
-		{
-			ft_error("fd, append file", 1);
-			return ;
-		}
-		ft_write_to_file(fd, parsed);
+		parsed->redir_out = parsed->redir_out->next;
 	}
 }
 void	ft_red_in(t_parse *parsed)
@@ -51,21 +40,13 @@ void	ft_red_in(t_parse *parsed)
 	int		fd;
 
 	if (!parsed || !parsed->redir_in)
-        return;
+        ft_error("red_in, parsed", 1);
 	filename = parsed->redir_in->filename;
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
 		ft_error("red_in, fd", 1);
-		return ;
-	}
-	if (dup2(fd, STDIN_FILENO) < 0)
-	{
-		close(fd);
-		return ;
-	}
-	if (close(fd))
-		return ;
+	if (close(fd) == -1)
+		ft_error("red_in, close", 1);
 }
 
 int	ft_create_file(t_parse *parsed)
@@ -74,9 +55,13 @@ int	ft_create_file(t_parse *parsed)
 	int		fd;
 
 	if (!parsed || !parsed->redir_out)
-        return (-1);
+        ft_error("create_file, parsed", 1);
 	filename = parsed->redir_out->filename;
+	if (!filename)
+		ft_error("filename, create file", 1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0666);
+	if (fd == -1)
+		ft_error("fd, create file", 1);
 	return (fd);
 }
 
@@ -94,8 +79,10 @@ void	ft_write_to_file(int fd, t_parse *parsed)
 		write(fd, &text[i], 1);
 		i++;
 	}
-	write(fd, "\n", 1);
-	close(fd);
+	if (write(fd, "\n", 1) == -1)
+		ft_error("write, write to file", 1);
+	if (close(fd) == -1)
+		ft_error("close, write to file", 1);
 }
 
 void	ft_append(t_parse *parsed)
@@ -105,5 +92,9 @@ void	ft_append(t_parse *parsed)
 	
 	filename = parsed->redir_out->filename;
 	fd = open(filename, O_CREAT | O_RDWR | O_APPEND, 0666);
+	if (fd == -1)
+		ft_error("fd, append file", 1);
 	ft_write_to_file(fd, parsed);
+	if (close(fd) == -1)
+		ft_error("close, append file", 1);
 }
